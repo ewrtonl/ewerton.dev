2935d6de357396856cbbbb59478e92adf3f13328
Ewerton <ewerton.lu8659@gmail.com>
2024-07-04 10:37:13 -0300

    update data fix

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,17 +1,26 @@
-import { getPosts } from "../_services/notion"
+import { getPosts } from "../_services/notion";
+
+export async function generateStaticParams() {
+  const posts = await getPosts();
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
+
+export const revalidate = 10;
 
 export default async function BlogHome() {
-    const posts = await getPosts()
-    return (
-        <div>
-            <h1>Blog</h1>
-            <ul>
-                {posts.map((post) => (
-                    <li key={post.id}>
-                        <a href={`/blog/${post.slug}`}>{post.title}</a>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+  const posts = await getPosts();
+  return (
+    <div>
+      <h1>Blog</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <a href={`/blog/${post.slug}`}>{post.title}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
