2dbb865fb714715183fe8a32532e2d252ffab401
Ewerton <ewerton.lu8659@gmail.com>
2024-07-04 19:04:59 -0300

    dynamic test

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,28 +1,19 @@
-import { getPosts } from "../_services/notion";
-
-export const fetchCache = 'force-no-store';
-
-export async function generateStaticParams() {
-  const posts = await getPosts();
-  return posts.map((post) => ({
-    slug: post.slug,
-  }));
-}
-
-export const revalidate = 1;
+import { getPosts } from "../_services/notion"
 
 export default async function BlogHome() {
-  const posts = await getPosts();
-  return (
-    <div>
-      <h1>Blog</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <a href={`/blog/${post.slug}`}>{post.title}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    const posts = await getPosts()
+    return (
+        <div>
+            <h1>Blog</h1>
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <a href={`/blog/${post.slug}`}>{post.title}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
+
+export const dynamic = 'force-dynamic';
